d5379ea456557f9e7b2979689ff0626ad483788f
TejashreeKharchane41 <tejashreenk14@gmail.com>
2025-08-01 21:19:01 +0530

    Delete Sql-queries/q10_top5_products.sql

--- Sql-queries/q10_top5_products.sql
+++
@@ -1,6 +0,0 @@
--- Q10. Rank the top 5 products by total quantity sold.
-
-select Category, Sub_Category, max(Quantity) as quantity
-from myshipment
-group by Category, Sub_Category
-order by quantity desc limit 5;
